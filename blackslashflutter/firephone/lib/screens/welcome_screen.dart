7a1fd2f6686611347ef3740b0c56e5859a097970
yaonur <ya_onur@hotmail.com>
2023-06-28 11:53:00 +0300

    ss

---
+++ blackslashflutter/firephone/lib/screens/welcome_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Image.asset("assets/image1.png")],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
